fix fitness dims, make informational task init silent

search_space_compression.py
@@ -424,7 +424,7 @@ class FitnessPredictor(nn.Module):
     """
     Auxiliary predictor: maps concatentated graph and task latents (z_g, z_t) to fitness.
     """
-    def __init__(self, latent_dim, hidden_dim=64, fitness_dim=2):
+    def __init__(self, latent_dim, hidden_dim=64, fitness_dim=4):
         super().__init__()
         self.fc1 = nn.Linear(latent_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, fitness_dim)

tasks.py
@@ -42,7 +42,7 @@ class Task:
     def name(cls):
         return cls.__name__.replace('tasks.', '')
 
-    def __init__(self, metrics:List[Metric], num_samples:int, train_ratio:float, inputs:np.ndarray, outputs:np.ndarray):
+    def __init__(self, metrics:List[Metric], num_samples:int, train_ratio:float, inputs:np.ndarray, outputs:np.ndarray, silent=False):
         self.metrics = metrics
         self.num_samples = num_samples
         self.train_ratio = train_ratio
@@ -54,15 +54,20 @@ class Task:
         self.feature_functions.append(lambda _: self.train_data.num_input_features)
         self.feature_functions.append(lambda _: self.train_data.num_output_features)
         # TODO: include metrics in task feature_functions for the task embedding to have access
-        print('  Calculating Task Features')
+        if not silent:
+            print('  Calculating Task Features')
         self.features = []
-        print('    Training Data Inputs')
+        if not silent:
+            print('    Training Data Inputs')
         self.features.append([np.mean(func(self.train_data.inputs)) for func in self.feature_functions])
-        print('    Training Data Outputs')
+        if not silent:
+            print('    Training Data Outputs')
         self.features.append([np.mean(func(self.train_data.outputs)) for func in self.feature_functions])
-        print('    Validation Data Inputs')
+        if not silent:
+            print('    Validation Data Inputs')
         self.features.append([np.mean(func(self.valid_data.inputs)) for func in self.feature_functions])
-        print('    Validation Data Outputs')
+        if not silent:
+            print('    Validation Data Outputs')
         self.features.append([np.mean(func(self.valid_data.outputs)) for func in self.feature_functions])
         self.check_partition_similarity()
 
@@ -85,18 +90,18 @@ class Task:
 
 
 class RegressionTask(Task):
-
     @staticmethod
-    def random_init():
+    def random_init(num_samples=None, silent=False):
         return RegressionTask(
             true_dims=random.randint(1, 10),
             observed_dims=random.randint(1, 10),
             metrics=[MSELoss()],
-            num_samples=random.randint(100, 1000),
-            train_ratio=max(.5, min(random.random(), .7))
+            num_samples=num_samples if num_samples is not None else random.randint(100, 1000),
+            train_ratio=max(.5, min(random.random(), .7)),
+            silent=silent
         )
 
-    def __init__(self, true_dims:int, observed_dims:int, metrics:List, num_samples:int, train_ratio:float):
+    def __init__(self, true_dims:int, observed_dims:int, metrics:List, num_samples:int, train_ratio:float, silent=False):
         self.true_dims = true_dims
         self.observed_dims = observed_dims
         inputs, outputs = generate_complex_regression_data(num_samples, true_dims, observed_dims)
@@ -120,11 +125,11 @@ class RegressionTask(Task):
             skew,
             kurtosis,
         ]
-        super().__init__(metrics, num_samples, train_ratio, inputs, outputs)
+        super().__init__(metrics, num_samples, train_ratio, inputs, outputs, silent)
 
 class HurstTargetTimeSeriesTransformTask(Task):
     @staticmethod
-    def random_init():
+    def random_init(num_samples=None, silent=False):
         num_features = random.randint(1, 10)
         return HurstTargetTimeSeriesTransformTask(
             mean=np.random.randn(num_features)*100,
@@ -134,8 +139,9 @@ class HurstTargetTimeSeriesTransformTask(Task):
             series_length=random.randint(100, 200),
             num_features=num_features,
             metrics=[MSELoss()],
-            num_samples=random.randint(100, 500),
-            train_ratio=max(.5, min(random.random(), .7))
+            num_samples=num_samples if num_samples is not None else random.randint(100, 500),
+            train_ratio=max(.5, min(random.random(), .7)),
+            silent=silent
         )
 
     def __init__(self,
@@ -147,7 +153,8 @@ class HurstTargetTimeSeriesTransformTask(Task):
         num_features:int,
         metrics:List[Metric],
         num_samples:int,
-        train_ratio:float
+        train_ratio:float,
+        silent=False
     ):
         self.mean = mean
         self.stdev = stdev
@@ -157,7 +164,7 @@ class HurstTargetTimeSeriesTransformTask(Task):
         self.num_features = num_features
         sequences = [generate_fbm_sequence(mean, stdev, hurst_target, fbm_length, num_features, series_length) for _ in range(num_samples)]
         outputs = [generate_fbm_sequence(mean, stdev, hurst_target, fbm_length, num_features, series_length) for _ in range(num_samples)]
-        self,feature_functions = [
+        self.feature_functions = [
             *SERIES_STATS,
             lambda _: self.mean,
             lambda _: self.stdev,
@@ -166,11 +173,11 @@ class HurstTargetTimeSeriesTransformTask(Task):
             lambda _: self.series_length,
             lambda _: self.num_features,
         ]
-        super().__init__(metrics, num_samples, train_ratio, np.array(sequences), np.array(outputs))
+        super().__init__(metrics, num_samples, train_ratio, np.array(sequences), np.array(outputs), silent)
 
 TASK_TYPE_TO_CLASS = {
     RegressionTask.name(): RegressionTask,
     HurstTargetTimeSeriesTransformTask.name(): HurstTargetTimeSeriesTransformTask,
 }
-TASK_FEATURE_DIMS = {n: 4*len(c.random_init().feature_functions) for n, c in TASK_TYPE_TO_CLASS.items()}
+TASK_FEATURE_DIMS = {n: 4*len(c.random_init(num_samples=4, silent=True).feature_functions) for n, c in TASK_TYPE_TO_CLASS.items()}
 TASK_TYPE_TO_INDEX = {k: i for i, k in enumerate(TASK_FEATURE_DIMS.keys())}